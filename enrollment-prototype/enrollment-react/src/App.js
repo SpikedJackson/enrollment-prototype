f9345e7dd1177e3377c1d6e29bd755e51c9da839
alexmcmullen80 <alex88.mcmullen@gmail.com>
2024-11-06 11:15:27 -0500

    added ability to switch between pages

--- enrollment-prototype/enrollment-react/src/App.js
+++ enrollment-prototype/enrollment-react/src/App.js
@@ -4,14 +4,25 @@ import Header from './Header';
 import SearchBar from './SearchBar';
 import Planner from './Planner';
 import PlanCourse from './PlanCourse';
+import Schedule from './Schedule.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header text = "Enrollment"/>
       <div className="container">
-        <PlanCourse />
+        <Router>
+          <Routes>
+            <Route path="/" element={<PlanCourse />} />
+            <Route path="/schedule" element={<Schedule />} />
+          </Routes>
+        </Router>
       </div>
+      
+      
+        {/* <PlanCourse /> */}
+      
 
     </div>
   );
