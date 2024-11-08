85b3f2a71be99b2c3e8e80349fd632f78f830ec3
alexmcmullen80 <alex88.mcmullen@gmail.com>
2024-11-07 23:16:22 -0500

    added ability to add courses to schedule

--- enrollment-prototype/enrollment-react/src/Schedule.jsx
+++ enrollment-prototype/enrollment-react/src/Schedule.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Schedule() {
+    const location = useLocation();
+    const {selectedCourses} = location.state || {selectedCourses: []}; //make it an empty array if no data passed
+
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    const timeSlots = ['9:00 - 10:00', '10:00 - 11:00', '11:00 - 12:00', '12:00 - 1:00', '1:00 - 2:00', '2:00 - 3:00', '3:00 - 4:00', '4:00 - 5:00' ];
   return (
     <>
       <div className="container">
         <div className="box">
           <div className="box1-container">
-            <div className="courses-header">
+            {/* <div className="courses-header">
               Courses
-            </div>
+            </div> */}
+
             <div className="course-container1">
-              <div className="course-box">
-                <p className="course-code">★ COMPSCI 2LC3</p>
-              </div>
-              <div className="course-box">
-                <p className="course-code">★ COMPSCI 2ME3</p>
-              </div>
-              <div className="course-box">
-                <p className="course-code">★ COMPSCI 2C03</p>
-              </div>
-              <div className="course-box">
-                <p className="course-code">ASTRON 2B03</p>
-              </div>
+            {selectedCourses.length === 0 ? (
+                <p>No courses selected yet.</p>
+              ) : (
+                selectedCourses.map((course, index) => (
+                  <div key={index} className="course-box">
+                    <p className="course-code">★ {course.code}</p>
+                  </div>
+                ))
+              )}
             </div>
           </div>
         </div>
@@ -37,62 +40,121 @@ function Schedule() {
             <div className="day-column">Thursday</div>
             <div className="day-column">Friday</div>
 
-            {/* Time Slots (9:00 AM - 5:00 PM) with Set Course Assignments */}
             <div className="time-column">9:00 - 10:00</div>
-            <div className="course-slot compsci-2lc3">★ COMPSCI 2LC3</div>
+            {selectedCourses.length === 0 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot light-blue">★ {selectedCourses[0].code}</div>
+              )}
             <div className="course-slot"></div>
             <div className="course-slot"></div>
-            <div className="course-slot compsci-2me3">★ COMPSCI 2ME3</div>
+            {selectedCourses.length <= 1 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot light-pink">★ {selectedCourses[1].code}</div>
+              )}
             <div className="course-slot"></div>
 
             <div className="time-column">10:00 - 11:00</div>
             <div className="course-slot"></div>
-            <div className="course-slot compsci-2c03">★ COMPSCI 2C03</div>
-            <div className="course-slot astron-2b03">ASTRON 2B03</div>
+            {selectedCourses.length <= 2 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot light-orange">{selectedCourses[2].code}</div>
+              )}
+            {selectedCourses.length <= 3 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot dark-green">{selectedCourses[3].code}</div>
+              )}
             <div className="course-slot"></div>
             <div className="course-slot"></div>
 
             <div className="time-column">11:00 - 12:00</div>
-            <div className="course-slot compsci-2me3">★ COMPSCI 2ME3</div>
+            {selectedCourses.length <= 1 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot light-pink">★ {selectedCourses[1].code}</div>
+              )}
             <div className="course-slot"></div>
-            <div className="course-slot astron-2b03">ASTRON 2B03</div>
-            <div className="course-slot compsci-2lc3">★ COMPSCI 2LC3</div>
+            {selectedCourses.length <= 4 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot red">{selectedCourses[4].code}</div>
+              )}
+            {selectedCourses.length === 0 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot light-blue">★ {selectedCourses[0].code}</div>
+              )}
             <div className="course-slot"></div>
 
             <div className="time-column">12:00 - 1:00</div>
             <div className="course-slot"></div>
             <div className="course-slot"></div>
             <div className="course-slot"></div>
-            <div className="course-slot compsci-2c03">★ COMPSCI 2C03</div>
-            <div className="course-slot astron-2b03">ASTRON 2B03</div>
+            {selectedCourses.length <= 2 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot light-orange">{selectedCourses[2].code}</div>
+              )}
+            {selectedCourses.length <= 3 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot dark-green">{selectedCourses[3].code}</div>
+              )}
 
             <div className="time-column">1:00 - 2:00</div>
-            <div className="course-slot compsci-2lc3">★ COMPSCI 2LC3</div>
+            {selectedCourses.length === 0 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot light-blue">★ {selectedCourses[0].code}</div>
+              )}
             <div className="course-slot"></div>
-            <div className="course-slot compsci-2me3">★ COMPSCI 2ME3</div>
+            {selectedCourses.length <= 1 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot light-pink">★ {selectedCourses[1].code}</div>
+              )}
             <div className="course-slot"></div>
             <div className="course-slot"></div>
 
             <div className="time-column">2:00 - 3:00</div>
             <div className="course-slot"></div>
-            <div className="course-slot compsci-2c03">★ COMPSCI 2C03</div>
+            {selectedCourses.length <= 2 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot light-orange">{selectedCourses[2].code}</div>
+              )}
             <div className="course-slot"></div>
-            <div className="course-slot astron-2b03">ASTRON 2B03</div>
+            {selectedCourses.length <= 4 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot red">{selectedCourses[4].code}</div>
+              )}
             <div className="course-slot"></div>
 
             <div className="time-column">3:00 - 4:00</div>
             <div className="course-slot"></div>
-            <div className="course-slot compsci-2me3">★ COMPSCI 2ME3</div>
             <div className="course-slot"></div>
-            <div className="course-slot compsci-2lc3">★ COMPSCI 2LC3</div>
+            <div className="course-slot"></div>
+            {selectedCourses.length <= 3 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot dark-green">{selectedCourses[3].code}</div>
+              )}
             <div className="course-slot"></div>
 
             <div className="time-column">4:00 - 5:00</div>
             <div className="course-slot"></div>
-            <div className="course-slot compsci-2c03">★ COMPSCI 2C03</div>
             <div className="course-slot"></div>
             <div className="course-slot"></div>
-            <div className="course-slot astron-2b03">ASTRON 2B03</div>
+            <div className="course-slot"></div>
+            {selectedCourses.length <= 4 ? (
+                <div className="course-slot"></div>
+              ) : (
+                <div className="course-slot red">{selectedCourses[4].code}</div>
+              )}
           </div>
         </div>
       </div>
