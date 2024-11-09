99c4e3ca03c260b26c26a240e5fa5bd3ca0d780b
SpikedJackson <jacksontimewell@gmail.com>
2024-11-09 15:09:54 -0500

    planner styling

--- planner-prototype/planner-react/src/CourseTable.jsx
+++ planner-prototype/planner-react/src/CourseTable.jsx
@@ -1,46 +1,67 @@
 import React from 'react';
 
 const CourseTable = ({ courses, onCourseRemove, allYears, currentYear, years }) => {
+    const displayCourse = (course) => {
+        return (
+            <div className="display-course">
+                {course.code} - {course.name}
+                <button onClick={() => onCourseRemove(course)} style={{ marginLeft: '10px' }}>
+                    <img src={require('./images/delete.png')} className="button-image" />
+                </button>
+            </div>
+        )
+    }
+
     return (
         <div className="course-container">
             {courses.length > 0 ? (
                 <>
-                {allYears ? <>
-                    <table>
-                    {years.map((year) => (
-                        <tr>
-                            <td>
-                                {year}
-                            </td>
-                            <td>
-                                {courses.map((course) => (
-                                    (course.year === year) &&
-                                        <div className="course-box">
-                                            {course.code} - {course.name} - {course.year}
-                                                <button onClick={() => onCourseRemove(course)} style={{ marginLeft: '10px'}}>
-                                                    <img src={require('./images/delete.png')} className="button-image"/>
-                                                </button>
+                    {allYears ? <>
+                        <div id="All Years" class="tab-content">
+                            legend: <font color="#3a8ce0">■</font> = planned, <font color="#e7f473">■</font> = currently enrolled, <font color="#38e150">■</font> = completed
+                            <div class="planner">
+                                {years.map((year) => (
+                                    <>
+                                        <div class="year-row">{year}</div>
+                                        <div class="course-row">
+                                            {courses.map((course) => (
+                                                (course.year === year) &&
+                                                <div class="course-slot-planner planned">
+                                                    {displayCourse(course)}
+                                                </div>
+                                            ))}
                                         </div>
+                                    </>
                                 ))}
-                            </td>
-                        </tr>
-                    ))}
-                    </table>
-                </> : <>
-                    <ul>
-                    {courses.map((course) => (
-                        (course.year === currentYear) && 
-                        <li key={course.code}>
-                            <div className="course-box">
-                                {course.code} - {course.name} - {course.year}
-                                    <button onClick={() => onCourseRemove(course)} style={{ marginLeft: '10px'}}>
-                                        <img src={require('./images/delete.png')} className="button-image"/>
-                                    </button>
                             </div>
-                        </li>
-                    ))}
-                </ul>
-                </>}
+                        </div>
+                    </> : <>
+                        <div class="tab-content">
+                            <div class="planner">
+                                <div class="year-row">Planned</div>
+                                {courses.map((course) => (
+                                    (course.year === currentYear /**&& course.planned === true */) &&
+                                    <div class="course-slot-planner planned">
+                                        {displayCourse(course)}
+                                    </div>
+                                ))}
+                                <div class="year-row">Enrolled</div>
+                                {courses.map((course) => (
+                                    (course.year === currentYear /**&& course.enrolled === true */) &&
+                                    <div class="course-slot-planner enrolled">
+                                        {displayCourse(course)}
+                                    </div>
+                                ))}
+                                <div class="year-row">Completed</div>
+                                {courses.map((course) => (
+                                    (course.year === currentYear /**&& course.completed === true */) &&
+                                    <div class="course-slot-planner completed">
+                                        {displayCourse(course)}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </>}
                 </>
             ) : (
                 <p>No courses selected.</p>
