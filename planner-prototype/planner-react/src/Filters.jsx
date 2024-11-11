c0561d3579b06eda63ea404eacb7d5fe93b24692
Jackson <43098221+SpikedJackson@users.noreply.github.com>
2024-11-11 12:01:05 -0500

    overhaul of planner react for styling and enhanced search

---
+++ planner-prototype/planner-react/src/Filters.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Filters = ({ selectedFilter, setSelectedFilter }) => {
+
+    return (
+        <div className="box3-container">
+            <div className="tab">
+                <button 
+                    onClick={() => setSelectedFilter('all') }
+                    className={selectedFilter === 'all' ? 'active' : ''}>
+                    All
+                </button>
+                <button 
+                    onClick={() => setSelectedFilter('required')}
+                    className={selectedFilter === 'required' ? 'active' : ''}>
+                    Required
+                </button>
+                <button 
+                    onClick={() => setSelectedFilter('electives')}
+                    className={selectedFilter === 'electives' ? 'active' : ''}>
+                    Electives
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Filters;
