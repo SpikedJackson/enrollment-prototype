8e8097b7c2546182c76cf0afd010077970633376
alexmcmullen80 <alex88.mcmullen@gmail.com>
2024-11-12 19:19:53 -0500

    more courses

--- final-prototype/src/colors.js
+++ final-prototype/src/colors.js
@@ -1,4 +1,4 @@
 
 
-const colors = ['#3CB371', '#FF1493', '#8A2BE2', '#17A398', '#FF8C00', '#1E90FF', '#EE6C4D']
+const colors = ['#3CB371', '#FF1493', '#8A2BE2', '#17A398', '#FF8C00', '#1E90FF', '#EE6C4D', '#6A5ACD', '#FFD700', '#DC143C', '#00BFFF']
 export default colors;
